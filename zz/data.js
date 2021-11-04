e62f1492f06c92a2fb26782edad23a484e4f669b
weideguo <2515717245@qq.com>
2021-11-05 00:35:52 +0800

    feature: web navigation

---
+++ zz/data.js
@@ -0,0 +1,68 @@
+/*
+左边导航
+通过名称跟左边关联
+*/
+left_info = [
+["常用推荐" ,[] ],
+["技术" ,["数据库","编程语言","前端","安全","技术杂项"] ],
+]
+
+
+/*
+右边每一个框
+四个字段：
+url         必须
+名称        必须
+logo        可选，为空则显示default.png
+详细说明    可选
+*/
+
+right_info = [
+["常用推荐", [[
+["http://www.sysu.edu.cn/","中山大学","./assets/images/logos/sysu.jpg", "为什么不去康乐园逛逛呢"],
+["https://weideguo.github.io/","github page","", ""], 
+],[],]],
+
+["数据库", [[
+["https://www.mysql.com/","mysql","", ""],
+["https://mariadb.org/","mariadb","", ""],
+["https://www.postgresql.org/","postgresql","", ""], 
+["https://www.mongodb.com/","mongodb","", ""], 
+["https://www.redis.com/","redis","", ""], 
+],[],]],
+
+["编程语言", [[
+["https://www.golang.org/","golang","", ""],
+["https://golang.google.cn/","golangcn","", ""],
+["https://www.python.org/","python","", ""],
+["https://www.cppreference.com/","c++参考文档","",""],
+["https://developer.mozilla.org/","mdn js","","mozilla的js参考文档"],
+],[],]],
+
+["前端", [[
+["https://vuejs.org/","vue","", ""],
+["https://cn.vuejs.org/","vuecn","", ""],
+["https://www.iviewui.com/","iviewui","", ""],
+["https://element.eleme.io/","element","", ""],
+
+],[],]],
+
+["安全", [[
+["http://cve.mitre.org/","cve","", "Common Vulnerabilities & Exposures"],
+
+
+],[],]],
+
+["技术杂项", [[
+["https://db-engines.com/","DB-Engines","", ""],
+
+],[],]],
+
+["前沿研究", [[
+["https://www.vldb.org/","vldb",  "", "Very Large Data Base"],
+["https://sigmod.org/",  "sigmod","", "ACM Special Interest Group on Management of Data"],
+["http://www.kdd.org/",  "sigkdd","", "ACM Special Interest Group on Knowledge Discovery and Data Mining"]
+],[],]],
+
+]
+
